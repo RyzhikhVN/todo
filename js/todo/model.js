353687d9c4c6e0b9021a11544f84eed850e7dd2a
RyzhikhVN <72731823+RyzhikhVN@users.noreply.github.com>
2023-06-15 02:00:02 +0300

    Update model.js

--- js/todo/model.js
+++ js/todo/model.js
@@ -21,11 +21,6 @@ export default class Model {
     addTask(text) {
         let id = 1
 
-        // let dataE = Object.keys(this.tasks)
-        // for (let idE of dataE) {
-        //    id = idE
-        // }
-
         if (this.tasks.length > 0) {
             this.tasks.length + 1
             id = this.tasks[this.tasks.length - 1]['id'] + 1
@@ -66,11 +61,13 @@ export default class Model {
     // удаление задач
     remoteTask(ind) {
         const index = this.tasks.indexOf(ind)
+        
+        console.log("удалить из массива заявок индекс =", index)
 
-        // console.log("удалить из массива заявок индекс =", index)
-
-        this.tasks.splice(index, 1) 
-        this.saveStorage()
+        this.tasks.splice(index, 1) // уд. 1 элем ,начиная с index 
+        this.saveStorage()      
     }
 }
+
+
 
