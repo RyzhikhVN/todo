0276dc1798792e69ea530465ee5fbb66d4538029
RyzhikhVN <72731823+RyzhikhVN@users.noreply.github.com>
2023-06-14 18:21:14 +0300

    Update controller.js
    correcting the deletion of an element

--- js/todo/controller.js
+++ js/todo/controller.js
@@ -18,52 +18,23 @@ view.elemRender.form.addEventListener("submit", function (e) {
 view.elemRender.listTask.addEventListener("click", function (e) {
 
     if (e.target.getAttribute("type") === "checkbox") {
-        // нужно обратиться вверх к родителю с id
+
         const elemId = e.target.closest(".todo-item").dataset.id
         const taskId = model.complTasks(elemId)
 
         // поменяем статус на done или active
         model.doneTask(taskId)
-        // подчеркивание елемента со статусом done
         view.veryfyTask(taskId)
     }
 
     // удаление по кнопке
     if (e.target.hasAttribute("data-dell")) {
         const removeId = e.target.closest(".todo-item").dataset.id
-        const removeEl = model.remoteTask(removeId)
+        const removeEl = model.complTasks(removeId)
+        model.removeTask(removeEl)
         view.removeElement(removeEl)
     }
-
 })
 
-// console.log(model.tasks)
-    // model.remoteTask(model.tasks[0])
-
-
-
-// model.addTask("сделать портфолио")
-// model.addTask("посмотреть вакансии")
-// model.addTask("дополнить резюме")
-// model.addTask("изучить Git")
-// model.addTask("изучить React")
-
-// изменяем задачу
-//  model.doneTask(model.tasks[0])
-
-// удаляем задачу
-//  model.remoteTask(model.tasks[2])
-
-// отображение модели задач
-// console.log(model.tasks)
-
-// сохраняем в хранилище
-
-// view.render(model.tasks)
-
-// дописать saveStorage во всех этапах в model
-
-// model.tasks.status("done")[1]
-// view.renderHtml()
 
 
